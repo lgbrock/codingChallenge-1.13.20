d857cb9bbbaa9db0befb53d248149ea355203b68
Logan Brock <36110596+lgbrock@users.noreply.github.com>
2021-01-13 20:07:18 -0600

    js update

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
-const names = ['logan', 'mary', 'trukey dog'];
-console.log(names);
+//Please create a function that takes in an array. The function should console.log() the sum of the first value in the array and the last value in the array
+
+let numbers = [1, 2, 3, 4];
+
+function run(numbers) {
+    let firstNum = numbers[0];
+    let lastNum = numbers[numbers.length - 1];
+    let result = (firstNum + lastNum);
+    console.log(result);
+}
+run(numbers)
